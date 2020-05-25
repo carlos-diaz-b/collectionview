ac4052a5af28db6be4018b7694a28a5f1da103a4
Carlos Diaz <ca.diaz@hotmail.com>
2020-05-24 19:01:06 -0700

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Carlos andres Diaz bravo  on 2020-05-24.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
